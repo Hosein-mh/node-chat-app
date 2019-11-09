09a606f032616861e3f6cb65e905cc824850e1fe
Hosein <homoh817@gmail.com>
2019-11-09 19:42:31 +0330

    add initial configs of socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express = require('express');
-
-var app = express();
+const http = require('http');
+const socketIo = require('socket.io');
 
 const publicDir = path.join(__dirname , '../public');
 const Port = process.env.Port || 3000;
 
+var app = express();
+var server = http.createServer(app);
+
+
 //configs
 app.use(express.static(publicDir));
 
-app.listen(Port , () => {
+//socket.io configs
+const io = socketIo(server);
+io.on('connection' , (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect' , () => {
+        console.log('user disconnected')
+    })
+})
+
+server.listen(Port , () => {
     console.log(`app is up on ${Port}`);
 })
